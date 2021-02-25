cd36a535a0bb9ce3d75c1dfbde21c98443c46878
Evandro oliveira <vandoaparecido@hotmail.com>
2021-02-24 23:40:34 -0300

    Estrutura do challengeNotActive

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -6,6 +6,7 @@ import { Profile } from '../components/Profile';
 import styles from '../styles/pages/Home.module.css';
 
 import Head from 'next/head';
+import { ChallengeBox } from '../components/ChallengeBox';
 
 export default function Home() {
   return (
@@ -23,7 +24,9 @@ export default function Home() {
            <CompletedChallenges/> 
            <Countdown />          
           </div>
-         <div></div>
+         <div>
+           <ChallengeBox />
+         </div>
        </section>
   </div>
   )
