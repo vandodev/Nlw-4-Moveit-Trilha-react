4ed5fc61f32d74cad3a2e76f6afd9f252e23cf2f
Evandro oliveira <vandoaparecido@hotmail.com>
2021-02-25 00:24:38 -0300

    Finalizando html do challengeBox

--- src/components/ChallengeBox.tsx
+++ src/components/ChallengeBox.tsx
@@ -4,7 +4,36 @@ export function ChallengeBox() {
     const hasActiveChallenge = true;
   return(
     <div className={styles.challengeBoxContainer}>
-        {hasActiveChallenge ? (<div/>):(
+        {hasActiveChallenge ? (
+            <div className={styles.challengeActive}>
+                <header>Ganhe 400 xp</header>
+
+                <main>
+                    <img src="icons/body.svg" alt=""/>
+                    <strong>Novo desafio</strong>
+                    <p>Levante e faça uma caminhada</p>
+                </main>
+
+                <footer>
+                <button 
+                    className={styles.challengeFailedButton} 
+                    type="button"
+                   
+                >
+                     Falhei
+                </button>
+
+                <button 
+                    className={styles.challengeSucceededButton} 
+                    type="button"
+                >
+                    Completei
+                </button>
+
+                </footer>
+
+            </div>
+        ):(
             <div className={styles.challengeNotActive}>
             <strong>Finalize um ciclo para receber um desafio</strong>
             <p>
