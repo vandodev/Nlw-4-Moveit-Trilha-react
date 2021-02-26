64e8090dba2bb7bfbe93ccf323409eaca40796aa
Evandro oliveira <vandoaparecido@hotmail.com>
2021-02-26 01:47:01 -0300

    Alterando barra de progresso altoáticamente

--- src/components/ExperienceBar.tsx
+++ src/components/ExperienceBar.tsx
@@ -5,13 +5,19 @@ import styles from '../styles/components/ExperienceBar.module.css';
 export function ExperienceBar() {
   const { currentExperience, experienceToNextLevel} = useContext(ChallengesContext);
   
+  const percentToNextNevel = Math.round(currentExperience * 100) / experienceToNextLevel;
+
   return (
       <header className={styles.experienceBar}>
         <span>0 xp</span>
         <div>
-          <div style={{ width: '50%' }} />
+          <div style={{ width: `${percentToNextNevel}%` }} />
   
-          <span className={styles.currentExperience} style={{ left: '50%' }}>{currentExperience} xp</span>
+          <span className={styles.currentExperience} 
+          style={{ left: `${percentToNextNevel}%` }}>
+            {currentExperience} xp
+          </span>
+          
         </div>
         <span>{experienceToNextLevel} xp</span>
       </header>
--- src/contexts/ChallengesContext.tsx
+++ src/contexts/ChallengesContext.tsx
@@ -29,7 +29,7 @@ export const ChallengesContext = createContext({} as ChallengesContextData);
 export function ChallengesProvider({children}: ChallengesProviderProps){
 
     const [level, setLevel] = useState(1);
-    const [currentExperience, setCurrentExperience] = useState(0);
+    const [currentExperience, setCurrentExperience] = useState(20);
     const [challengesCompleted, setChallengesCompleted] = useState(0);
 
     const [activeChallenge, setActiveChallenge] = useState(null)
