8188e70715ad766137f5a48767e6f4f9e225fb56
Evandro oliveira <vandoaparecido@hotmail.com>
2021-03-08 01:21:41 -0300

    Commit

--- src/components/Profile.tsx
+++ src/components/Profile.tsx
@@ -6,9 +6,9 @@ export function Profile() {
   const { level } = useContext(ChallengesContext);
   return (
     <div className={styles.profileContainer}>
-      <img src="https://github.com/diego3g.png" alt="Diego Fernandes"/>
+      <img src="https://github.com/vandodev.png" alt="Evandro Oliveira"/>
       <div>
-        <strong>Diego Fernandes</strong>
+        <strong>Evandro Oliveira</strong>
         <p>
           <img src="icons/level.svg" alt="Level"/>
           Level:{level}
